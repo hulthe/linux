d5d836f9b45c6e1bbfa550edb5af5080014e2069
Joakim Hulthe <joakim@hulthe.net>
2022-02-15 13:29:37 +0100

    exfat_rust: Register stub filesystem thing

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -125,7 +125,7 @@ pub trait KernelModule: Sized + Sync {
 /// Equivalent to `THIS_MODULE` in the C API.
 ///
 /// C header: `include/linux/export.h`
-pub struct ThisModule(*mut bindings::module);
+pub struct ThisModule(pub *mut bindings::module);
 
 // SAFETY: `THIS_MODULE` may be used from all threads within a module.
 unsafe impl Sync for ThisModule {}
