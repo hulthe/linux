7287aad222fa22721ca0f0e5c0da0198ea266a47
Joakim Hulthe <joakim@hulthe.net>
2022-02-18 13:47:13 +0100

    exfat_rust: Add sb_bread abstraction

--- fs/exfat_rust/external.rs
+++ fs/exfat_rust/external.rs
@@ -1,13 +1,35 @@
 //! stuff that should probably be moved to kernel lib
 
-use kernel::bindings::{__bread_gfp, buffer_head, sector_t, super_block, ___GFP_MOVABLE};
+use core::slice;
+use kernel::bindings::{__bread_gfp, __brelse, buffer_head, sector_t, super_block, ___GFP_MOVABLE};
 use kernel::c_types::c_uint;
 
-#[inline(always)]
-pub(crate) fn sb_bread(sb: &mut super_block, block: sector_t) -> Option<&mut buffer_head> {
-    unsafe {
-        // TODO: is this the right ___GFP_MOVABLE? (two vs three underscores)
-        // SAFETY: i have no idea
-        __bread_gfp(sb.s_bdev, block, sb.s_blocksize as c_uint, ___GFP_MOVABLE).as_mut()
+pub(crate) struct BufferHead {
+    ptr: *mut buffer_head,
+}
+
+impl BufferHead {
+    pub(crate) fn block_read(sb: &mut super_block, block: sector_t) -> Option<Self> {
+        let ptr = unsafe {
+            // TODO: is this the right ___GFP_MOVABLE? (two vs three underscores)
+            // SAFETY: i have no idea
+            __bread_gfp(sb.s_bdev, block, sb.s_blocksize as c_uint, ___GFP_MOVABLE).as_mut()?
+        };
+
+        Some(BufferHead { ptr })
+    }
+
+    pub(crate) fn bytes(&self) -> &[u8] {
+        unsafe {
+            let bh = &*self.ptr;
+            slice::from_raw_parts(bh.b_data as *const u8, bh.b_size)
+        }
+    }
+}
+
+impl Drop for BufferHead {
+    fn drop(&mut self) {
+        // Try to free the buffer
+        unsafe { __brelse(self.ptr) }
     }
 }
