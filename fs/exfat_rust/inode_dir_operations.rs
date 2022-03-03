a0f7a0d9caa1bdb8f7d36de573d8563ab41dfdd2
Joakim Hulthe <joakim@hulthe.net>
2022-03-03 12:04:26 +0100

    inode_dir_operations: Export stub functions

--- fs/exfat_rust/inode_dir_operations.rs
+++ fs/exfat_rust/inode_dir_operations.rs
@@ -1,12 +1,58 @@
-use kernel::bindings::inode_operations as InodeOperations;
+use kernel::bindings::{
+    dentry as DEntry, inode as Inode, inode_operations as InodeOperations, umode_t,
+    user_namespace as UserNamespace,
+};
+use kernel::c_types::{c_int, c_uint};
+
+extern "C" fn exfat_create(
+    _mnt_userns: *mut UserNamespace,
+    _dir: *mut Inode,
+    _dentry: *mut DEntry,
+    _mode: umode_t,
+    _excl: bool,
+) -> c_int {
+    todo!("exfat_create"); // TODO: implement me
+}
+
+extern "C" fn exfat_lookup(_dir: *mut Inode, _dentry: *mut DEntry, _flags: c_uint) -> *mut DEntry {
+    todo!("exfat_lookup"); // TODO: implement me
+}
+
+extern "C" fn exfat_mkdir(
+    _mnt_userns: *mut UserNamespace,
+    _dir: *mut Inode,
+    _dentry: *mut DEntry,
+    _mode: umode_t,
+) -> c_int {
+    todo!("exfat_mkdir"); // TODO: implement me
+}
+
+extern "C" fn exfat_rmdir(_dir: *mut Inode, _dentry: *mut DEntry) -> c_int {
+    todo!("exfat_rmdir"); // TODO: implement me
+}
+
+extern "C" fn exfat_unlink(_dir: *mut Inode, _dentry: *mut DEntry) -> c_int {
+    todo!("exfat_unlink"); // TODO: implement me
+}
+
+extern "C" fn exfat_rename(
+    _mnt_userns: *mut UserNamespace,
+    _old_dir: *mut Inode,
+    _old_dentry: *mut DEntry,
+    _new_dir: *mut Inode,
+    _new_dentry: *mut DEntry,
+    _flags: c_uint,
+) -> c_int {
+    todo!("exfat_rename"); // TODO: implement me
+}
 
 pub(crate) static DIR_INODE_OPERATIONS: InodeOperations = InodeOperations {
-    create: None,  // TODO:
-    lookup: None,  // TODO:
-    mkdir: None,   // TODO:
-    rmdir: None,   // TODO:
-    unlink: None,  // TODO:
-    rename: None,  // TODO:
+    create: Some(exfat_create),
+    lookup: Some(exfat_lookup),
+    mkdir: Some(exfat_mkdir),
+    rmdir: Some(exfat_rmdir),
+    unlink: Some(exfat_unlink),
+    rename: Some(exfat_rename),
     setattr: None, // TODO:
     getattr: None, // TODO:
 
