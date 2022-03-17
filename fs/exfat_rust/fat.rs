43a0cc56ac88cf859511f477b179c1b5226684db
Joakim Hulthe <joakim@hulthe.net>
2022-03-17 13:13:21 +0100

    Fix FatChainReader handling of zeroed fat entries

--- fs/exfat_rust/fat.rs
+++ fs/exfat_rust/fat.rs
@@ -1,7 +1,7 @@
 use crate::external::BufferHead;
-use crate::superblock::SuperBlock;
+use crate::superblock::{BootSectorInfo, SuperBlock};
 use core::mem::size_of;
-use kernel::{Error, Result};
+use kernel::{pr_err, pr_info, Error, Result};
 
 pub(crate) type ClusterIndex = u32;
 
@@ -10,13 +10,15 @@ const FAT_ENTRY_BAD: u32 = 0xFFFFFFF7;
 const FAT_ENTRY_EOF: u32 = 0xFFFFFFFF;
 
 pub(crate) struct FatChainReader<'a> {
+    boot: &'a BootSectorInfo,
     sb: &'a SuperBlock,
     next: Option<ClusterIndex>,
 }
 
 impl<'a> FatChainReader<'a> {
-    pub(crate) fn new(sb: &'a SuperBlock, index: ClusterIndex) -> Self {
+    pub(crate) fn new(boot: &'a BootSectorInfo, sb: &'a SuperBlock, index: ClusterIndex) -> Self {
         FatChainReader {
+            boot,
             sb,
             next: Some(index),
         }
@@ -32,20 +34,29 @@ impl Iterator for FatChainReader<'_> {
         let entry_size = size_of::<ClusterIndex>();
         let total_byte_offset = entry_size * index as usize;
         let sector_size = self.sb.s_blocksize as u64;
-        let sector = total_byte_offset as u64 / sector_size;
+        let sector = self.boot.fat1_start_sector + total_byte_offset as u64 / sector_size;
         let byte_offset = total_byte_offset % sector_size as usize;
 
         let block = BufferHead::block_read(self.sb, sector)?;
         let bytes = &block.bytes()[byte_offset..][..entry_size];
 
         let next = ClusterIndex::from_le_bytes(bytes.try_into().unwrap());
+
+        pr_info!("fatchain index={}, start_sector={}, entry_size={}, sector_size={}, sector={}, byte_offset={}, next={:?}", index, self.boot.fat1_start_sector, entry_size, sector_size, sector, byte_offset, next);
+
         match next {
-            FAT_ENTRY_FREE | FAT_ENTRY_EOF => self.next = None,
+            FAT_ENTRY_FREE => self.next = Some(index + 1),
+            FAT_ENTRY_EOF => self.next = None,
             FAT_ENTRY_BAD => {
                 self.next = None;
                 return Some(Err(Error::EIO));
             }
-            _ => self.next = Some(next),
+            _ if next > index => self.next = Some(next),
+            _ => {
+                pr_err!("error: next FAT entry is smaller than current");
+                self.next = None;
+                return Some(Err(Error::EIO));
+            }
         }
 
         Some(Ok(index))
